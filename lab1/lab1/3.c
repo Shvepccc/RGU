74c9a1e5e200eeb6d662a6cccaf9a9fc80fc0675
Stepan Orlov <rfkfi345@gmail.com>
2024-10-13 17:30:14 +0300

    lab1: Task 2,10,13,14 are done

--- lab1/lab1/3.c
+++ lab1/lab1/3.c
@@ -10,9 +10,9 @@
 //int main() {
 //	int a = 2;
 //	int b = 4;
-//	printf("%d %d ", a, b);
+//	printf("Start data: %d %d\n", a, b);
 //
 //	change(&a, &b);
 //
-//	printf("%d %d ", a, b);
+//	printf("End data  : %d %d ", a, b);
 //}
