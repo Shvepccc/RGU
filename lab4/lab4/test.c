e96b57d3095f8d939b40f205a4f05209e4dd90d1
Shvepccc <rfkfi345@gmail.com>
2024-10-29 17:56:22 +0300

    lab4: task 7 is done

---
+++ lab4/lab4/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//int blablabla(int* n) {
+//	printf("%d", *n);
+//	return 0;
+//}
+
+int main2222() {
+	printf("%d", atoi(NULL));
+}
--- libs/memory.h
+++ libs/memory.h
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int cust_realloc(void** ptr, size_t size);
+int cust_realloc(void** ptr, size_t size) {
+	void* ptr2 = NULL;
+
+	ptr2 = (void*)realloc(*ptr, size);
+	if (ptr2 == NULL) {
+		return 1;
+	}
+	*ptr = ptr2;
+	return 0;
+}
 
 #endif
