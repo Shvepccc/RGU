6d24b24eb2e2d04bde87cd515e86ef7723c50a0b
Shvepccc <rfkfi345@gmail.com>
2026-03-17 17:38:19 +0300

    cr_hw_5: create generation random numbers

--- semester 4/crypt/cr_hw_5/src/2.cpp
+++ semester 4/crypt/cr_hw_5/src/2.cpp
@@ -5,6 +5,20 @@
 #include "../include/probabilistic_primality_test.hpp"
 #include "../include/modeular_arithmetic.hpp"
 
+uint64_t generate_random_uint64(uint64_t a, uint64_t b)
+{
+    static std::random_device rd;
+    static std::mt19937_64 gen(rd());
+    
+    if (a > b)
+    {
+        std::swap(a, b);
+    }
+    
+    std::uniform_int_distribution<uint64_t> dist(a, b);
+    return dist(gen);
+}
+
 class fermat_primality_test : public probabilistic_primality_test
 {
 private:
@@ -35,16 +49,24 @@ int main()
     std::cout << "=== Fermat Primality Test ===\n";
 
     uint64_t test_numbers[] = {2, 3, 4, 5, 7, 9, 11, 13, 15, 17, 19,
-        21, 23, 29, 341, 561, 1105, 1729};
+        21, 23, 29, 341, 561, 1105, 1729,
+        static_cast<uint64_t>(UINT64_MAX*0.9)};
 
-    for (uint64_t n : test_numbers)
+    // for (uint64_t n : test_numbers)
+    // {
+    //     bool is_prime_result = fermat_test.is_prime(n, 0.999);
+    //     std::cout << n << " is " << (is_prime_result ? "probably prime" : "composite") << "\n";
+    // }
+
+    for (int i = 0; i < 10; i++)
     {
+        uint64_t n = generate_random_uint64(static_cast<uint64_t>(UINT64_MAX*0.9), UINT64_MAX);
         bool is_prime_result = fermat_test.is_prime(n, 0.999);
         std::cout << n << " is " << (is_prime_result ? "probably prime" : "composite") << "\n";
     }
 
     std::cout << "\n=== Testing with different probabilities ===\n";
-    uint64_t n = 341;
+    uint64_t n = generate_random_uint64(static_cast<uint64_t>(UINT64_MAX*0.9), UINT64_MAX);
 
     for (double prob : {0.5, 0.75, 0.9, 0.99, 0.999})
     {
